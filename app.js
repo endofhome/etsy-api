2f1a520c6a475c4bb54cabf53789125e31e71e0e
DuskyShelf <dupsdale@gmail.com>
2016-02-16 11:38:19 +0000

    added request to handle api getter

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
-var https = require('https');
+var request = require('request');
+
+
 
 app.get('/', function (req, res) {
-  https.get('https://openapi.etsy.com/v2/listings/active?api_key=w49mqdq0fic46wosw2qa4gw4',
-    function(data) {
-      console.log(data.body);
+  request('https://openapi.etsy.com/v2/listings/active?api_key=w49mqdq0fic46wosw2qa4gw4', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(body)
     }
-  );
+  })
 });
 
 app.listen(3000, function () {
